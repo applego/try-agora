e2a735e2f79b6210e87bc150bff35706f9f126d0
applego <applegorillappa@gmail.com>
2021-02-07 01:56:22 +0900

    add gitignore

--- public_org/secrets.js
+++
@@ -1,9 +0,0 @@
-// Project Management > Edit > Basic Info > App ID
-// https://console.agora.io/projects
-export const appId = "471c61e8ceb348ef802ddca84ded590b";
-
-// You can name this
-export const channel = "asuka_no_heya";
-
-// Project Management > Edit > Features > Generate temp token
-export const token = "006471c61e8ceb348ef802ddca84ded590bIABnpwWT8zKJBcfWQeSZI/GLaU/Jj/5c2s19OcTNzj/RiJ30BwIAAAAAEABwjq6PvlcdYAEAAQC7Vx1g";
