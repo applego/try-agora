5689e096d7fcd4156ad1f79435f0ebf11b83496b
applego <applegorillappa@gmail.com>
2021-02-07 02:56:58 +0900

    💡:split components

--- src/App.js
+++ src/App.js
@@ -1,133 +1,224 @@
+function Navbar() {
+  return (
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="https://bulma.io">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            width="112"
+            height="28"
+          />
+        </a>
+        <a
+          role="button"
+          className="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item">Home</a>
+          <a className="navbar-item">Documentation</a>
+          <div className="navbar-item has-dropdown is-hoverable">
+            <a className="navbar-link">More</a>
+            <div className="navbar-dropdown">
+              <a className="navbar-item">About</a>
+              <a className="navbar-item">Jobs</a>
+              <a className="navbar-item">Contact</a>
+              <hr className="navbar-divider" />
+              <a className="navbar-item">Report an issue</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a className="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a className="button is-light">Log in</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+function Header() {
+  return (
+    <header className="hero is-warning is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Try Agora</h1>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+function Join() {
+  return (
+    <div>
+      <button className="button is-link is-rounded" id="join">
+        ▶️ Join
+      </button>
+    </div>
+  );
+}
+
+function Leave() {
+  return (
+    <div>
+      <button className="button is-link is-rounded is-light" id="leave">
+        ⏹ Leave
+      </button>
+    </div>
+  );
+}
+
+function Publish() {
+  return (
+    <div>
+      <button className="button is-warning is-rounded" id="publish">
+        🔊 Publish(unmute)
+      </button>
+      {/* <button class="button" title="Disabled button" disabled>Disabled</button> */}
+    </div>
+  );
+}
+
+function Unpublish() {
+  return (
+    <div>
+      <button className="button is-warning is-rounded is-light" id="unpublish">
+        🔇 Unpublish(mute)
+      </button>
+    </div>
+  );
+}
+
+function UserId() {
+  return (
+    <p>
+      Your ID: <span id="userId"></span>
+    </p>
+  );
+}
+
+function Participants() {
+  return (
+    <div>
+      <p>
+        Participants (<span id="numOfParticipants">0</span>):
+      </p>
+      <ul id="participantList"></ul>
+    </div>
+  );
+}
+
+function Image() {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image">
+          <img
+            src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
+            alt="cute dog"
+          />
+        </figure>
+      </div>
+    </div>
+  );
+}
+
+function Gallery() {
+  return (
+    <div className="columns is-vcentered is-multiline">
+      <div className="column is-3">
+        <Image />
+      </div>
+    </div>
+  );
+}
+
+function Buttons() {
+  return (
+    <div className="columns">
+      <div className="column">
+        <Join />
+      </div>
+      <div className="column">
+        <Leave />
+      </div>
+      <div className="column">
+        <Publish />
+      </div>
+      <div className="column">
+        <Unpublish />
+      </div>
+    </div>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      <section className="section">
+        <div className="container">
+          <Buttons />
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <UserId />
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <Participants />
+        </div>
+      </section>
+      <section className="section">
+        <div className="container">
+          <Gallery />
+        </div>
+      </section>
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <a href="https://console.agora.io/">https://console.agora.io/</a>
+        </p>
+        <p>
+          <a href="https://docs.agora.io/en/Voice/start_call_audio_web_ng?platform=Web">
+            <p>&gt; Agora Voice Call Overview</p>
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
 function App() {
   return (
     <div>
-      <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="https://bulma.io">
-            <img
-              src="https://bulma.io/images/bulma-logo.png"
-              width="112"
-              height="28"
-            />
-          </a>
-          <a
-            role="button"
-            className="navbar-burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
-        <div id="navbarBasicExample" className="navbar-menu">
-          <div className="navbar-start">
-            <a className="navbar-item">Home</a>
-            <a className="navbar-item">Documentation</a>
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">More</a>
-              <div className="navbar-dropdown">
-                <a className="navbar-item">About</a>
-                <a className="navbar-item">Jobs</a>
-                <a className="navbar-item">Contact</a>
-                <hr className="navbar-divider" />
-                <a className="navbar-item">Report an issue</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <div className="buttons">
-                <a className="button is-primary">
-                  <strong>Sign up</strong>
-                </a>
-                <a className="button is-light">Log in</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-
-      <header className="hero is-warning is-bold">
-        <div className="hero-body">
-          <div className="container">
-            <h1 className="title">Try Agora</h1>
-          </div>
-        </div>
-      </header>
-      <main>
-        <section className="section">
-          <div className="container">
-            <div className="columns">
-              <div className="column">
-                <p>First column</p>
-                <button id="join">▶️ Join</button>
-              </div>
-              <div className="column">
-                <p>Second column</p>
-                <button id="leave">⏹ Leave</button>
-              </div>
-              <div className="column">
-                <p>Third column</p>
-                <button id="publish">🔊 Publish(unmute)</button>
-              </div>
-              <div className="column">
-                <p>Forth column</p>
-                <button id="unpublish">🔇 Unpublish(mute)</button>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section>
-          <div className="container">
-            <p>
-              Your ID: <span id="userId"></span>
-            </p>
-          </div>
-        </section>
-        <section>
-          <div className="container">
-            <p>
-              Participants (<span id="numOfParticipants">0</span>):
-            </p>
-            <ul id="participantList"></ul>
-          </div>
-        </section>
-
-        <section className="section">
-          <div className="container">
-            <div className="columns is-vcentered is-multiline">
-              <div className="column is-3">
-                <div className="card">
-                  <div className="card-image">
-                    <figure className="image">
-                      <img
-                        src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
-                        alt="cute dog"
-                      />
-                    </figure>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
-      <footer className="footer">
-        <div className="content has-text-centered">
-          <p>
-            <a href="https://console.agora.io/">https://console.agora.io/</a>
-          </p>
-          <p>
-            <a href="https://docs.agora.io/en/Voice/start_call_audio_web_ng?platform=Web">
-              <p>&gt;Agora Voice Call Overview</p>
-            </a>
-          </p>
-        </div>
-      </footer>
+      <Header />
+      <Navbar />
+      <Main />
+      <Footer />
     </div>
   );
 }
